c496988984a33acaa95f583dc1e8abb1e2a44ba2
dracojeff <jeffrousse@outlook.fr>
2021-03-19 16:38:48 -0400

    le deuxieme

---
+++ program.cs/program.cs/Class3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasseBatiment
+
+{
+    class Entrepot
+    {
+        private string _revetement;
+        private string _quaidedechargement;
+
+        public Entrepot(string revetement)
+        {
+            _revetement = revetement;
+        }
+        public void SetRevetement(string revetement)
+        {
+            _revetement = revetement;
+        }
+        public string GetRevetement()
+        {
+            return _revetement;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " revetement : " + _revetement;
+        }
+    }
+}
